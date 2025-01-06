a8183e723fee7320a4494b8d1cf3eaef37bfe6ee
Luke Logan <lukelogan@Lukes-MacBook-Air.local>
2025-01-06 06:07:38 -0500

    adds users and OPENapi

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SkillsModule } from './skills/skills.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [SkillsModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
